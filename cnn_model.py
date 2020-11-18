c251f39da563f4e9268ef410ae6bde0c67d2db18
Sapphire118 <2537218229@qq.com>
2020-11-18 22:09:23 +0800

    批量读取数据（2）

--- cnn_model.py
+++ cnn_model.py
@@ -41,3 +41,24 @@ class train_test(object):
             batch_labels = np.r_[batch_labels, self.labels[0:new_offset]]
             self.offset = new_offset
         return batch_images, batch_labels
+
+class digit_data(object):
+    def __init__(self):
+        self.train = train_test()
+        self.test = train_test()
+
+    def input_data(self):
+        mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+        images = np.r_[mnist.train.images, mnist.test.images]
+        labels = np.r_[mnist.train.labels, mnist.test.labels]
+        zeros = np.zeros((labels.shape[0], 6))
+        labels = np.c_[labels, zeros]
+        print("Loading the operators' datasets....")
+        op_images, op_labels = get_images_labels()
+        images, labels = np.r_[images, op_images], np.r_[labels, op_labels]
+        print("Generating the train_data and test_data....")
+        sss = StratifiedShuffleSplit(n_splits=16, test_size=0.15, random_state=23)
+        for train_index, test_index in sss.split(images, labels):
+            self.train.images, self.test.images = images[train_index], images[test_index]
+            self.train.labels, self.test.labels = labels[train_index], labels[test_index]
+
